3f4438faab059460f70814378ac99df9cc0cf94f
mao <1296193245@qq.com>
2023-12-08 10:37:44 +0800

    spring-boot-pdf-preview

---
+++ src/main/java/mao/springbootpdfpreview/SpringBootPdfPreviewApplication.java
@@ -0,0 +1,15 @@
+package mao.springbootpdfpreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPdfPreviewApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootPdfPreviewApplication.class, args);
+    }
+
+}
